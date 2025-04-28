9f631bf8000ee9d3fba6ccd7a4bc91ef314aeb8f
Darshan Babu Shrestha <151409165+DarshanBabuShrestha@users.noreply.github.com>
2025-04-28 14:18:04 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Allows access from any device in the network
-    port: 5173,       // Keep this port
-    strictPort: true,
-    hmr: {
-      clientPort: 5173, // Ensures hot module reload works
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        landing: 'landing.html',
+        audio: 'audio.html',
+        quiz: 'quiz.html'
+      }
     }
-  },
+  }
 });
